5852d5e21142c5fd5b7e1af2d1d61e35a807d519
usagi <kof2161705@stu.o-hara.ac.jp>
2025-09-03 14:47:03 +0900

    Revert "ボタン押すのとクリックするのどっちがいいかな"
    This reverts commit 1d19333aa82abeaa73eac2b8c375b9976124e75d.

--- Assets/stert program/start game.cs
+++ Assets/stert program/start game.cs
@@ -14,6 +14,6 @@ public class startgame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene");
     }
 }
