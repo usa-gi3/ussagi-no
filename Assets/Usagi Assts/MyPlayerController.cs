d80cfe9ffba8fccd4e978fae59f35533c9a25d30
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-09-16 10:02:30 +0900

    nemunemu

--- Assets/Usagi Assts/MyPlayerController.cs
+++ Assets/Usagi Assts/MyPlayerController.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -67,4 +67,4 @@ public class MyPlayerController : MonoBehaviour
         // à⁄ìÆé¿çs
         controller.Move(moveDirection * Time.deltaTime);
     }
-}*/
+}
