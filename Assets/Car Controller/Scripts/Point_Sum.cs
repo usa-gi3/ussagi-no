6f77ce96243a370d1eb7ce7b61e8f85f362fd4eb
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-09-25 11:27:47 +0900

    車走れる

---
+++ Assets/Car Controller/Scripts/Point_Sum.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point_Sum : MonoBehaviour
+{
+    public int score = 0;
+
+    public GameObject Carrot1;
+    public GameObject Carrot2;
+    public GameObject Carrot3;
+    public GameObject Carrot4;
+    public GameObject Carrot5;
+    public GameObject Carrot6;
+
+
+
+    void Start()
+    {
+        Carrot1.SetActive(false);
+        Carrot2.SetActive(false);
+        Carrot3.SetActive(false);
+        Carrot4.SetActive(false);
+        Carrot5.SetActive(false);
+        Carrot6.SetActive(false);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("point"))
+        {
+            score += 10;
+            Debug.Log("ポイントゲット！現在のスコア: " + score);
+
+            // オブジェクトを消したい場合はこれも追加
+            Destroy(other.gameObject);
+        }
+
+        if (score > 100)
+        {
+            Carrot1.SetActive(true);
+
+        }  
+        if (score > 200)
+        {
+            Carrot2.SetActive(true);
+
+        } 
+        if (score > 300)
+        { 
+         
+            Carrot3.SetActive(true);
+        }
+        
+        if (score > 400)
+        {
+            Carrot4.SetActive(true);
+        }
+        
+        if (score > 500)
+        {
+            Carrot5.SetActive(true);
+        }
+        
+        if (score > 600)
+        {
+            Carrot6.SetActive(true);
+        }
+    }
+
+
+}
