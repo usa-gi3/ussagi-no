b559b78c6da6db5358a97835bddef83f30616461
usagi <kof2161705@stu.o-hara.ac.jp>
2025-09-29 09:54:42 +0900

    クリックでマウスが消えないように

--- Assets/Usagi Assts/See.cs
+++ Assets/Usagi Assts/See.cs
@@ -9,7 +9,6 @@ public class See : MonoBehaviour
     public float Ysensityvity = 3f; // ç∂âEä¥ìx
 
     Quaternion cameraRot, characterRot;
-    bool cursorLock = true;
 
     // è„â∫âÒì]ÇÃêßå¿
     float minX = -90f, maxX = 90f;
@@ -36,22 +35,8 @@ public class See : MonoBehaviour
         transform.localRotation = characterRot;
         cam.transform.localRotation = cameraRot;
 
-        UpdateCursorLock();
     }
 
-    void UpdateCursorLock()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            cursorLock = false;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            cursorLock = true;
-        }
-
-        Cursor.lockState = cursorLock ? CursorLockMode.Locked : CursorLockMode.None;
-    }
 
     // è„â∫ÇÃâÒì]Çêßå¿Ç∑ÇÈä÷êî
     Quaternion ClampRotation(Quaternion q)
