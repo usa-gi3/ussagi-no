5c7b45edd08e880896ea3496553f40a68b157de9
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-10-01 15:11:05 +0900

    動いたので判定と乱数を実装中

---
+++ Assets/Usagi Assts/bar/OrderChecker.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+public class OrderChecker : MonoBehaviour
+{
+    public OrderGenerator orderGenerator;
+    public ObjectClickManager clickManager;
+
+    public void CheckOrder()
+    {
+        List<int> order = orderGenerator.GetOrder(); // 1桁目: フルーツ, 2〜4桁目: ドリンク
+        List<int> player = clickManager.GetFullHistory(); // プレイヤーの履歴（4件）
+
+        if (player.Count < 4)
+        {
+            Debug.Log("まだ入力が足りません");
+            return;
+        }
+
+        bool isMatch = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (order[i] != player[i])
+            {
+                isMatch = false;
+                break;
+            }
+        }
+
+        if (isMatch)
+        {
+            Debug.Log("注文通り！正解です！");
+        }
+        else
+        {
+            Debug.Log("注文と違います！");
+        }
+    }
+}
---
+++ Assets/Usagi Assts/bar/OrderGenerator.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+public class OrderGenerator : MonoBehaviour
+{
+    public List<int> currentOrder = new List<int>(); // 4桁の注文番号
+
+    void Start()
+    {
+        GenerateOrder();
+    }
+
+    public void GenerateOrder()
+    {
+        currentOrder.Clear();
+
+        // 1桁目：1（Apple）または 2（Carrot）
+        int fruit = Random.Range(1, 3); // 1〜2
+        currentOrder.Add(fruit);
+
+        // 2〜4桁目：1〜5のドリンク番号
+        for (int i = 0; i < 3; i++)
+        {
+            int drink = Random.Range(1, 6); // 1〜5
+            currentOrder.Add(drink);
+        }
+
+        Debug.Log("生成された注文: " + string.Join(", ", currentOrder));
+    }
+
+    public List<int> GetOrder()
+    {
+        return new List<int>(currentOrder);
+    }
+}
