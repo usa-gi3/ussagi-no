ecd5b028fd4bc5eb730a685d47cc9d5fb44b2610
usagi <kof2161705@stu.o-hara.ac.jp>
2025-10-08 11:39:22 +0900

    配置したけど抜けてたらごめーん

--- Assets/stert program/Mark.cs
+++ Assets/stert program/Mark.cs
@@ -1,23 +1,22 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mark : MonoBehaviour
 {
-    [SerializeField] private GameObject talkMark; // 会話可能マーク
+    [SerializeField] private GameObject EnterMark; // 会話可能マーク
     [SerializeField] private Camera mainCamera;   // メインカメラ
 
     void Start()
     {
-        if (talkMark != null) talkMark.SetActive(false);//マークを非表示に
+        if (EnterMark != null) EnterMark.SetActive(false);//マークを非表示に
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (talkMark != null) talkMark.SetActive(true); // 範囲に入ったらマーク表示
+            if (EnterMark != null) EnterMark.SetActive(true); // 範囲に入ったらマーク表示
         }
     }
 
@@ -25,15 +24,15 @@ public class Mark : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if (talkMark != null) talkMark.SetActive(false); // 範囲から出たら非表示
+            if (EnterMark != null) EnterMark.SetActive(false); // 範囲から出たら非表示
         }
     }
     private void LateUpdate()
     {
         // マークをカメラの方向に向ける
-        if (talkMark != null && talkMark.activeSelf)
+        if (EnterMark != null && EnterMark.activeSelf)
         {
-            talkMark.transform.LookAt(talkMark.transform.position + mainCamera.transform.forward);
+            EnterMark.transform.LookAt(EnterMark.transform.position + mainCamera.transform.forward);
         }
     }
 }
