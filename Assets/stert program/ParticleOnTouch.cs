9676c0731aeeed696d2e818c0a428163e77c68bb
usagi <kof2161705@stu.o-hara.ac.jp>
2025-10-15 11:49:22 +0900

    何が起きてるんだろ

---
+++ Assets/stert program/ParticleOnTouch.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOnTouch : MonoBehaviour
+{
+    [SerializeField] private GameObject particlePrefab; // パーティクルのプレハブ（ParticleSystem付き）
+    private GameObject spawnedParticle; // 生成されたパーティクルの参照
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && spawnedParticle == null)
+        {
+            // パーティクルを生成して再生
+            Vector3 spawnPos = transform.position + Vector3.up * 3f;
+            spawnedParticle = Instantiate(particlePrefab, spawnPos, Quaternion.identity);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && spawnedParticle != null)
+        {
+            // パーティクル停止
+            ParticleSystem ps = spawnedParticle.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                ps.Stop();
+            }
+
+            // 少し待ってから破壊（エフェクトが自然に消えるように）
+            Destroy(spawnedParticle, 1f);
+            spawnedParticle = null;
+        }
+    }
+}
