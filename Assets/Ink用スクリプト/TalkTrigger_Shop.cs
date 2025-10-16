daeed8f925c798f7b27a5f1f42aa8f8a7f36edc4
望月 柚菜 <kof2464737@stu.o-hara.ac.jp>
2025-10-16 15:06:08 +0900

    マップ作製中

---
+++ Assets/Ink用スクリプト/TalkTrigger_Shop.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class TalkTrigger_Shop : MonoBehaviour
+{
+    public InkController_Shop inkController;
+    private bool isPlayerNear = false;
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            inkController.StartDialogue();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            inkController.EndDialogue(); // ó£ÇÍÇΩÇÁUIÇï¬Ç∂ÇÈÅiîCà”Åj
+        }
+    }
+}
